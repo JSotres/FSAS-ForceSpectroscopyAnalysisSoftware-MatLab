a9ffa6e6de883365bb99547bf868aa0fb3e5ca74
Javier Sotres <jsotres@gmail.com>
2019-10-13 00:24:21 +0200

    Comments improved

--- FromRange2Indexes.m
+++ FromRange2Indexes.m
@@ -1,18 +1,19 @@
-function [IndMin,IndMax]=FromRange2Indexes(InputVector,MinValue,MaxValue)
+function [IndMin, IndMax] =...
+    FromRange2Indexes(InputVector, MinValue, MaxValue)
+% Provided and 1D array and a minimum and maximum values, retur the indices
+% corresponding to those values in the input vector
 
 SizeInputVector=max(size(InputVector));
 
 for i=1:SizeInputVector
-    IndMin=i;
-    InputVector(i);
+    IndMin = i;
     if InputVector(i)>MinValue
         break;
     end
 end
 
 for j=i:SizeInputVector
-    IndMax=j;
-    InputVector(j);
+    IndMax = j;
     if InputVector(j)>MaxValue
         break;
     end
