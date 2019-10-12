d26a877929ab7aa6f843173cfefea7a89bbce6c4
Javier Sotres <jsotres@gmail.com>
2019-10-13 01:08:54 +0200

    Comments improved

--- HertzXOffsetDetermination.m
+++ HertzXOffsetDetermination.m
@@ -23,19 +23,31 @@ function [E, x0, xData, yData] =...
 % Units of x0, xData and yData are [nm]
 % Units of E are GPa
 
+% It require arranging xData and Ydata in incremental
+% values of xData, which is done by calling the external
+% function "Ordenar" 
 [xData,Ydata] = Ordenar(xOriginal,yOriginal);
+
+% The function corresponding to the Hertz fit is defined
 Hertz_FitType = fittype(@(K2,Z0,x) -K2*x.^(2/3)-x+Z0);
+% And the fit is performed
 Fit1 = fit(xData, Ydata, Hertz_FitType, 'StartPoint',[0.5 20]);
 
 Coefficients = coeffvalues(Fit1);
 if Coefficients(1)>0
+    % If the K2 parameter is positive, the Younf modulus and contact
+    % position (output parametes) are assigned
     E = 3*(1-PoissonRatio^2)*kc/(4*R^(0.5)*Coefficients(1)^(3/2));
     x0 = Coefficients(2);
 else
+    % If the K2 parameter is negative, which will happen if the slope of
+    % the contact region is lower than the photodetector sensitivity, the
+    % output Young modulus and contact positions are assigned NaN values
     E = NaN;
     x0 = NaN;
 end
 
+% The output X and Y values of the Hertz fit are calculated
 xData = 0:0.1:max(xData);
 yData = -Coefficients(1)*xData.^(2/3)-xData;
 
--- Ordenar.m
+++ Ordenar.m
@@ -1,30 +1,24 @@
 function [xnew, ynew]=Ordenar(xold,yold)
+% Arranges xold and yold in incremental
+% values of xold 
 
-%matriz_temporal=load(matriz_desordenada);
+% number of rows of xold
+n_rows = max(size(xold));
 
-n_filas=max(size(xold));
-
-x=xold;
-y=yold;
-
-
-for i=2:n_filas
-    for j=i:-1:2
-        if x(j)<x(j-1)
-            x_temp=x(j-1);
-            y_temp=y(j-1);
-            x(j-1)=x(j);
-            y(j-1)=y(j);
-            x(j)=x_temp;
-            y(j)=y_temp;
+%performs the actual arrangement
+x = xold;
+y = yold;
+for i = 2:n_rows
+    for j = i:-1:2
+        if x(j) < x(j-1)
+            x_temp = x(j-1);
+            y_temp = y(j-1);
+            x(j-1) = x(j);
+            y(j-1) = y(j);
+            x(j) = x_temp;
+            y(j) = y_temp;
         end
     end
 end
-
-xnew=x;
-ynew=y;
-
-
-
-%plot(x,y,'o','MarkerSize',2);
-
+xnew = x;
+ynew = y;
